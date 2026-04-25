762478bf23ae92957212925b2e6a44828f1e7b10
sevinchoy <sevinchoyazimboevna@gmail.com>
2026-04-25 16:14:00 +0500

    feat: train callback hamda async functions

--- train.js
+++ train.js
@@ -8,27 +8,46 @@ const list = [
     "endi dam oling foydasi yoq endi", //60
 ]
 
- function maslahatBering (a, callback) {
-    if(typeof a !== "number") callback("insert a number", null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
+ async function maslahatBering (a) {
+    if(typeof a !== "number") throw new Error("insert a number");
+    else if (a <= 20) return list[0];
+    else if (a > 20 && a <= 30) return list[1];
+    else if (a > 30 && a <= 40) return list[2];
+    else if (a > 40 && a <= 50) return list[3];
+    else if (a > 50 && a <= 60) return list[4];
     else {
-        setTimeout(function () {
-            callback(null, list[5]);
-        }, 5000)
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            }, 5000);
+            // setInterval(() => {
+            //     resolve(list[5]);
+            // }, 1000);
+        });
+        // setTimeout(function () {
+        //     return list[5];
+        // }, 5000);
     }
  }
 
+ //then/catch
+//  console.log("passed here 0");
+//  maslahatBering(25)
+//  .then((data) => {
+//    console.log("javob:", data);
+//  })
+//  .catch((err) => {
+//   console.log("ERROR:", err);
+//  });
+//  console.log("passed here 1");  
 
- console.log('passed here 0');
- maslahatBering(10,(err, data) => {
-     if(err) console.log('ERROR', err);
-     else {
-      console.log('javob:',data);
-     }
-    //  console.log('javob:',data);
- });
- console.log('passed here 1');
+//asyn/await
+async function run() {
+    let javob = await maslahatBering(65);
+    console.log(javob);
+    javob = await maslahatBering(31);
+    console.log(javob);
+    javob = await maslahatBering(41);
+    console.log(javob); 
+}
+run(); 
